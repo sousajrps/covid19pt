bc18c867274da73019b410081d8a48c63ebf7a71
João Sousa <joaosousa.developer@gmail.com>
2021-04-19 20:56:07 +0100

    Adds vaccination weekly repository

--- app/src/main/java/com/sousajrps/covid19pt/vaccination/VaccinationRepository.kt
+++ app/src/main/java/com/sousajrps/covid19pt/vaccination/VaccinationRepository.kt
@@ -1,9 +1,10 @@
 package com.sousajrps.covid19pt.vaccination
 
 import com.sousajrps.covid19pt.remote.models.Vaccination
+import com.sousajrps.covid19pt.remote.models.VaccinationWeekly
 import io.reactivex.Single
 
 interface VaccinationRepository {
-    fun getRemoteVaccination(): Single<List<Vaccination>>
-    fun getLocalVaccination(): Single<List<Vaccination>>
+    fun getAllRemoteData(): Single<Pair<List<Vaccination>, List<VaccinationWeekly>>>
+    fun getAllLocalData(): Single<Pair<List<Vaccination>, List<VaccinationWeekly>>>
 }
--- app/src/main/java/com/sousajrps/covid19pt/vaccination/VaccinationRepositoryImpl.kt
+++ app/src/main/java/com/sousajrps/covid19pt/vaccination/VaccinationRepositoryImpl.kt
@@ -2,23 +2,54 @@ package com.sousajrps.covid19pt.vaccination
 
 import com.sousajrps.covid19pt.remote.RemoteData
 import com.sousajrps.covid19pt.remote.mappers.VaccinationMapper
+import com.sousajrps.covid19pt.remote.mappers.VaccinationWeeklyMapper
 import com.sousajrps.covid19pt.remote.models.Vaccination
+import com.sousajrps.covid19pt.remote.models.VaccinationWeekly
 import com.sousajrps.covid19pt.sharedPreferences.AppSharedPreferences
 import io.reactivex.Single
 
 class VaccinationRepositoryImpl(
     private val remoteData: RemoteData,
     private val appSharedPreferences: AppSharedPreferences,
-    private val vaccinationMapper: VaccinationMapper
+    private val vaccinationMapper: VaccinationMapper,
+    private val vaccinationWeeklyMapper: VaccinationWeeklyMapper
 ) : VaccinationRepository {
 
-    override fun getRemoteVaccination(): Single<List<Vaccination>> =
+    override fun getAllRemoteData(): Single<Pair<List<Vaccination>, List<VaccinationWeekly>>> =
+        Single.zip(
+            getRemoteVaccination(),
+            getRemoteVaccinationWeekly(),
+            { first: List<Vaccination>,
+              second: List<VaccinationWeekly> ->
+                Pair(first, second)
+            })
+
+    override fun getAllLocalData(): Single<Pair<List<Vaccination>, List<VaccinationWeekly>>> =
+        Single.zip(
+            getLocalVaccination(),
+            getLocalVaccinationWeekly(),
+            { first: List<Vaccination>,
+              second: List<VaccinationWeekly> ->
+                Pair(first, second)
+            })
+
+    private fun getRemoteVaccination(): Single<List<Vaccination>> =
         remoteData.getRemoteCovid19PtVaccination()
             .doOnSuccess { appSharedPreferences.covid19PtVaccination = it }
             .onErrorResumeNext { appSharedPreferences.getLocalCovid19PtVaccinationSingle() }
             .map { vaccinationMapper.mapCovid19VaccinationRawData(it) }
 
-    override fun getLocalVaccination(): Single<List<Vaccination>> =
+    private fun getLocalVaccination(): Single<List<Vaccination>> =
         appSharedPreferences.getLocalCovid19PtVaccinationSingle()
             .map { vaccinationMapper.mapCovid19VaccinationRawData(it) }
+
+    private fun getRemoteVaccinationWeekly(): Single<List<VaccinationWeekly>> =
+        remoteData.getRemoteCovid19PtVaccinationWeekly()
+            .doOnSuccess { appSharedPreferences.covid19PtVaccinationWeekly = it }
+            .onErrorResumeNext { appSharedPreferences.getLocalCovid19PtVaccinationWeeklySingle() }
+            .map { vaccinationWeeklyMapper.mapVaccinationWeeklyRawData(it) }
+
+    private fun getLocalVaccinationWeekly(): Single<List<VaccinationWeekly>> =
+        appSharedPreferences.getLocalCovid19PtVaccinationWeeklySingle()
+            .map { vaccinationWeeklyMapper.mapVaccinationWeeklyRawData(it) }
 }
